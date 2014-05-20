b29f72cf9d241722996eb2af6c0a92ae9e7b5bdb
Vee Satayamas <vsatayamas@gmail.com>
2014-05-20 14:33:46 +0700

    first commit

---
+++ lib/reina/stream_parser.rb
@@ -0,0 +1,93 @@
+# Copyright (C) 2014 Vee Satayamas
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE X CONSORTIUM BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require "whittle"
+
+module Reina
+  class Lu
+    attr_reader :surface, :analyses
+    
+    def initialize
+      @analyses = []
+    end
+  end
+  
+  class AmbiLu < Lu
+    def initialize(surface, analyses)
+      super()
+      @surface = surface
+      @analyses = analyses
+    end
+  end
+  
+  class UnkLu < Lu
+    def initialize(surface)
+      super()
+      @surface = surface
+    end
+  end
+
+  class NonLu < Lu
+    def initialize(surface)
+      super()
+      @surface = surface
+    end
+  end
+  
+  class StreamParser < Whittle::Parser
+    rule(:wsp => /\s+/).skip!
+    rule(:surface => /\^[^\^^\$^\<][^\$^<^\/]*/)
+    rule(:text => /[^\/^\^^\$^\<][^\^]*/)
+    rule(:elu => /\$/)
+    rule(:tag => /<\w+>/)
+    rule(:lemma => /\/[^\*^\$^<]+/)
+    rule(:unk => /\/\*[^\$]+/)
+
+    rule(:tags) do |r|
+      r[:tags, :tag].as {|tags, tag| 
+        tags + [tag]
+      }
+      r[:tag].as {|t| [t[1..-2]]}
+    end
+    
+    rule(:analysis) do |r|
+      r[:lemma, :tags].as {|lemma, tags| {tags: tags, lemma: lemma[1..-1]}}
+    end
+    
+    rule(:analyses) do |r|
+      r[:analyses, :analysis].as {|analyses, analysis| analyses + [analysis]}
+      r[:analysis].as {|analysis| [analysis]}
+    end
+
+    rule(:lu) do |r|
+      r[:surface, :analyses, :elu].as {|surface, analyses, _| 
+        AmbiLu.new(surface[1..-1], analyses)}
+      r[:surface, :unk, :elu].as {|surface, _, _| UnkLu.new(surface[1..-1])}
+      r[:text].as {|txt| NonLu.new(txt)}
+    end
+    
+    rule(:stream) do |r|
+      r[:stream, :lu].as {|lu_lst, lu| lu_lst + [lu]}
+      r[:lu].as {|lu| [lu]}
+    end 
+    start(:stream)
+  end  
+end
---
+++ reina.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'reina'
+  s.version     = '0.0.2'
+  s.date        = '2014-05-20'
+  s.summary     = "A toolset for Apertium written in Ruby"
+  s.description = "Reina.rb is a toolset for Apertium written in Ruby"
+  s.authors     = ["Vee Satayamas"]
+  s.email       = 'v.satayamas@gmail.com'
+  s.files       = ["lib/reina/stream_parser.rb"]
+  s.homepage    =
+    'https://github.com/veer66/reinarb'
+  s.license       = 'MIT'
+  s.add_dependency("whittle", "0.0.8")
+end
