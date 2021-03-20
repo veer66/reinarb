695983d5ec4ff74c4559585a83db8ee83f50be93
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2021-03-20 04:40:19 +0100

    Misc formatting to ldix_loader.rb

--- lib/reina/ldix_loader.rb
+++ lib/reina/ldix_loader.rb
@@ -4,8 +4,10 @@ module Reina
       file.map do |line|
         line.chomp.split(/:|:[<>]:/).map do |col|
           toks = col.split(/(<\w+>)/)
-          {headword: toks[0],
-            tags: toks[1..-1].map{|t| t[1..-2]}}
+          {
+            headword: toks[0],
+            tags: toks[1..-1].map{ |t| t[1..-2] }
+          }
         end
       end
     end
